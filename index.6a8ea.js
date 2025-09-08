2599cdf912c181e2f57c730ad85712e4815815c5
上帝的梦 <1597691532@qq.com>
2025-09-08 18:51:42 +0800

    1

--- index.6a8ea.js
+++ index.6a8ea.js
@@ -1,4 +1,4 @@
-System.register(["./application.aa3e7.js"], function (_export, _context) {
+System.register(["./application.fad4e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
