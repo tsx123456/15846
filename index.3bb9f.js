00e347dcd648366dc198887a45412859f06f61b4
上帝的梦 <1597691532@qq.com>
2025-09-11 18:35:05 +0800

    1

--- index.3bb9f.js
+++ index.3bb9f.js
@@ -1,4 +1,4 @@
-System.register(["./application.fad4e.js"], function (_export, _context) {
+System.register(["./application.4607f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
