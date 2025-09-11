847c4d1374df45b738b125d964f973feb6329be4
上帝的梦 <1597691532@qq.com>
2025-09-11 19:13:30 +0800

    1

--- index.0591d.js
+++ index.0591d.js
@@ -1,4 +1,4 @@
-System.register(["./application.0c34a.js"], function (_export, _context) {
+System.register(["./application.c9c22.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
